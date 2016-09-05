a7baeb431449e86f42d0bca3d745b1db4d39fe43
jberthiaume1138 <joenyc23@gmail.com>
2016-09-05 02:26:01 -0400

    added basic handlebars functionality

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 require('./db/connect');
 var express = require('express');
+var exphbs = require('express-handlebars');
+var path = require('path');
 var bodyParser = require('body-parser');
 var itemRoutes = require('./routes/item');
 var app = express();
 
 app.use(bodyParser.json());
-app.use(express.static('public'));
+// app.use(express.static('public'));
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine','handlebars');
+app.set('views', __dirname + '/views');
+app.use(express.static(path.join(__dirname,'/public')));
 
 app.use('/', itemRoutes);
+
+app.get('/', function(req, res) {
+    res.render('home');
+});
+
 app.use('*', function(req, res) {
     res.status(404).json({ message: 'Not Found' });
 });
