46d7674bf8d7f6ad354cac23018219ea42977165
jberthiaume1138 <joenyc23@gmail.com>
2016-09-26 20:53:13 -0400

    Update db model, add status field - boolean default false

--- models/item.js
+++ models/item.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 var ItemSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    status: { type: Boolean, "default": false}
+    status: {type: Boolean, "default": false}
 });
 
 var Item = mongoose.model('Item', ItemSchema);
